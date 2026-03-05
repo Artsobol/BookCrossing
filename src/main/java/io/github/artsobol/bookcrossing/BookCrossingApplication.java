190f82d463a323547de9a342c1f3f854817e0895
Artsobol <artsobol05@yandex.ru>
2026-03-05 14:33:07 +0300

    initial commit

---
+++ src/main/java/io/github/artsobol/bookcrossing/BookCrossingApplication.java
@@ -0,0 +1,13 @@
+package io.github.artsobol.bookcrossing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrossingApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BookCrossingApplication.class, args);
+    }
+
+}

af226e168fbd7e5ed0ee8d53096434208bb1e741
Artsobol <artsobol05@yandex.ru>
2026-03-06 14:07:43 +0300

    build: add jwt dependencies, enable configuration, build application.yaml

--- src/main/java/io/github/artsobol/bookcrossing/BookCrossingApplication.java
+++ src/main/java/io/github/artsobol/bookcrossing/BookCrossingApplication.java
@@ -2,8 +2,10 @@ package io.github.artsobol.bookcrossing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BookCrossingApplication {
 
     static void main(String[] args) {
